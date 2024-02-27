f341a02c4b5c069c8e8f63c269bed9997eafd04f
johannaeunice <belemejohannaeunice@gmail.com>
2024-02-27 23:00:24 +0100

    income page done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [activeLink, setActiveLink] = useState('');
   const isHomePage = location.pathname === '/';
+  const protectedPaths = ['/dashboard', '/categories', '/transactions', '/contact', '/expenses', '/income'];
 
   // Update document title based on current location
   useEffect(() => {
@@ -16,23 +16,17 @@ const Navbar = () => {
 
   // Handle navigation link clicks
   const handleLinkClick = (event, link) => {
-    // Confirm navigation for certain links if not on home page
-    if (!isHomePage && ['/signup', '/login'].includes(link)) {
-      const confirmed = window.confirm(`You are about to leave the current page. Do you want to proceed to ${link}?`);
+    // Check if user is on protected page and wants to navigate away
+    if (protectedPaths.includes(location.pathname) && !protectedPaths.includes(link)) {
+      const confirmed = window.confirm(`You are about to leave the current page. Do you want to proceed to ${link} page?`);
       if (!confirmed) {
         event.preventDefault(); // Prevent default behavior
         return;
       }
     }
+
     // Logout user by removing the x-auth-token
     sessionStorage.removeItem('x-auth-token');
-    setActiveLink(link);
-  };
-
-  // Handle dropdown option click
-  const handleDropdownOptionClick = (path) => {
-    navigate(path);
-    setActiveLink('Transactions'); // Assuming 'Transactions' is the main link
   };
 
   return (
@@ -43,15 +37,15 @@ const Navbar = () => {
             <>
               {/* Home link */}
               <li style={styles.navItem}>
-                <NavLink exact to="/" style={styles.link} onClick={(event) => handleLinkClick(event, 'Home')} activeStyle={activeLink === 'Home' ? styles.activeLink : null}>Home</NavLink>
+                <NavLink exact to="/" style={styles.link} onClick={(event) => handleLinkClick(event, 'Home')}>Home</NavLink>
               </li>
               {/* Signup link */}
               <li style={styles.navItem}>
-                <NavLink to="/signup" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign Up')} activeStyle={activeLink === 'Sign Up' ? styles.activeLink : null}>Sign Up</NavLink>
+                <NavLink to="/signup" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign Up')}>Sign Up</NavLink>
               </li>
               {/* Login link */}
               <li style={styles.navItem}>
-                <NavLink to="/login" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign In')} activeStyle={activeLink === 'Sign In' ? styles.activeLink : null}>Sign In</NavLink>
+                <NavLink to="/login" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign In')}>Sign In</NavLink>
               </li>
             </>
           )}
@@ -60,38 +54,31 @@ const Navbar = () => {
             <>
               {/* Home link */}
               <li style={styles.navItem}>
-                <NavLink exact to="/" style={styles.link} onClick={handleLinkClick} activeStyle={activeLink === 'Home' ? styles.activeLink : null}>Home</NavLink>
+                <NavLink exact to="/" style={styles.link} onClick={(event) => handleLinkClick(event, 'Home')}>Home</NavLink>
               </li>
               {/* Dashboard link */}
               <li style={styles.navItem}>
-                <NavLink to="/dashboard" style={styles.link} activeStyle={activeLink === 'Dashboard' ? styles.activeLink : null}>Dashboard</NavLink>
+                <NavLink to="/dashboard" style={styles.link}>Dashboard</NavLink>
               </li>
               {/* Categories link */}
               <li style={styles.navItem}>
-                <NavLink to="/categories" style={styles.link} activeStyle={activeLink === 'Category' ? styles.activeLink : null}>Categories</NavLink>
+                <NavLink to="/categories" style={styles.link}>Categories</NavLink>
               </li>
-              {/* Transactions dropdown */}
+              {/* Transactions link */}
               <li style={styles.navItem}>
-                <div style={styles.dropdown}>
-                  <button className="text-white" style={styles.link} onClick={() => handleDropdownOptionClick('/transactions')}>Transactions</button>
-                  <div style={styles.dropdownContent}>
-                    <NavLink to="/transactions" style={styles.dropdownLink} onClick={() => handleDropdownOptionClick('/transactions')} activeClassName="font-bold">Transactions</NavLink>
-                    <NavLink to="/income" style={styles.dropdownLink} onClick={() => handleDropdownOptionClick('/income')} activeClassName="font-bold">Income</NavLink>
-                    <NavLink to="/expenses" style={styles.dropdownLink} onClick={() => handleDropdownOptionClick('/expenses')} activeClassName="font-bold">Expenses</NavLink>
-                  </div>
-                </div>
+                <NavLink to="/transactions" style={styles.link}>Transactions</NavLink>
               </li>
               {/* Contact Us link */}
               <li style={styles.navItem}>
-                <NavLink to="/contact" style={styles.link} activeStyle={activeLink === 'Contact Us' ? styles.activeLink : null}>Contact Us</NavLink>
+                <NavLink to="/contact" style={styles.link}>Contact Us</NavLink>
               </li>
               {/* Signup link */}
               <li style={styles.navItem}>
-                <NavLink to="/signup" style={styles.link} onClick={handleLinkClick} activeStyle={activeLink === 'Sign Up' ? styles.activeLink : null}>Sign Up</NavLink>
+                <NavLink to="/signup" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign Up')}>Sign Up</NavLink>
               </li>
               {/* Login link */}
               <li style={styles.navItem}>
-                <NavLink to="/login" style={styles.link} onClick={handleLinkClick} activeStyle={activeLink === 'Sign In' ? styles.activeLink : null}>Sign In</NavLink>
+                <NavLink to="/login" style={styles.link} onClick={(event) => handleLinkClick(event, 'Sign In')}>Sign In</NavLink>
               </li>
             </>
           )}
@@ -119,29 +106,6 @@ const styles = {
     textDecoration: 'none',
     transition: 'color 0.3s ease',
     padding: '10px',
-  },
-  activeLink: {
-    fontWeight: 'bold',
-    textDecoration: 'underline',
-  },
-  dropdown: {
-    position: 'relative',
-    display: 'inline-block',
-  },
-  dropdownContent: {
-    display: 'none',
-    position: 'absolute',
-    backgroundColor: '#f9f9f9',
-    minWidth: '160px',
-    boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)',
-    zIndex: '1',
-  },
-  dropdownLink: {
-    color: 'black',
-    padding: '12px 16px',
-    textDecoration: 'none',
-    display: 'block',
-    transition: 'background-color 0.3s ease',
   }
 };
 
